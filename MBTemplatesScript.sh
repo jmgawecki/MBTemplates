0693c2ed60e83e43df03fc25bc9fea149c903518
Jakub Gawecki <jmgawecki@icloud.com>
2023-03-05 19:02:06 +0000

    update the script

--- MBTemplatesScript.sh
+++ MBTemplatesScript.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
 EmptyMBXCTestCase=(
-    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/Empty%20MBXCTestCase%20File.xctemplate/TemplateIcon.png" 
-    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/Empty%20MBXCTestCase%20File.xctemplate/___FILEBASENAME___Tests.swift" 
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/Empty%20MBXCTestCase%20File.xctemplate/TemplateIcon.png"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/Empty%20MBXCTestCase%20File.xctemplate/___FILEBASENAME___Tests.swift"
     "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/Empty%20MBXCTestCase%20File.xctemplate/TemplateInfo.plist"
 )
 
 ViewModelTestswithMocks=(
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/___FILEBASENAME___ViewModelTests.swift"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/___FILEBASENAME___CoordinatorMock.swift"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/___FILEBASENAME___ViewControllerMock.swift"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/TemplateInfo.plist"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/TemplateIcon.png"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/___FILEBASENAME___ViewModelTests.swift"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/___FILEBASENAME___CoordinatorMock.swift"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/___FILEBASENAME___ViewControllerMock.swift"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/TemplateInfo.plist"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Test/View%20Model%20Tests%20with%20Mocks.xctemplate/TemplateIcon.png"
 )
 
 ScrollableViewController=(
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20View%20Controller.xctemplate/___FILEBASENAME___ViewController.swift"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20View%20Controller.xctemplate/TemplateInfo.plist"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20View%20Controller.xctemplate/TemplateIcon.png"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20View%20Controller.xctemplate/___FILEBASENAME___ViewController.swift"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20View%20Controller.xctemplate/TemplateInfo.plist"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20View%20Controller.xctemplate/TemplateIcon.png"
 )
 
 ScrollableVCwithViewModel=(
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20VC%20with%20View%20Model.xctemplate/___FILEBASENAME___ViewController.swift"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20VC%20with%20View%20Model.xctemplate/___FILEBASENAME___ViewModel.swift"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20VC%20with%20View%20Model.xctemplate/TemplateInfo.plist"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20VC%20with%20View%20Model.xctemplate/TemplateIcon.png"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20VC%20with%20View%20Model.xctemplate/___FILEBASENAME___ViewController.swift"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20VC%20with%20View%20Model.xctemplate/___FILEBASENAME___ViewModel.swift"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20VC%20with%20View%20Model.xctemplate/TemplateInfo.plist"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Scrollable%20VC%20with%20View%20Model.xctemplate/TemplateIcon.png"
 )
 
 Factory=(
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Factory.xctemplate/___FILEBASENAME___Factory.swift"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Factory.xctemplate/TemplateInfo.plist"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Factory.xctemplate/TemplateIcon.png"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Factory.xctemplate/___FILEBASENAME___Factory.swift"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Factory.xctemplate/TemplateInfo.plist"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Factory.xctemplate/TemplateIcon.png"
 )
 
 Coordinator=(
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Coordinator.xctemplate/___FILEBASENAME___Coordinator.swift"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Coordinator.xctemplate/TemplateInfo.plist"
-"https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Coordinator.xctemplate/TemplateIcon.png"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Coordinator.xctemplate/___FILEBASENAME___Coordinator.swift"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Coordinator.xctemplate/TemplateInfo.plist"
+    "https://raw.githubusercontent.com/jmgawecki/MBTemplates/main/Moneybox%20-%20Save%20and%20Invest/Coordinator.xctemplate/TemplateIcon.png"
 )
 
 main_destination="/Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File Templates/Moneybox - Save and Invest/"
@@ -50,11 +50,11 @@ fetch_files() {
 
     for FILE in "${FILES[@]}"; do
         FILENAME=$(basename "$FILE")
-
+        echo
         echo "File name $FILENAME"
         echo "Fetched from $FILE"
         echo "Destination path: $DESTINATION_PATH"
-
+        echo
         sudo curl -J -o "$DESTINATION_PATH/$FILENAME" "$FILE"
     done
 }
@@ -65,3 +65,7 @@ fetch_files ScrollableViewController[@] "$main_destination" "Scrollable View Con
 fetch_files ScrollableVCwithViewModel[@] "$main_destination" "Scrollable VC with View Model.xctemplate"
 fetch_files Factory[@] "$main_destination" "Factory.xctemplate"
 fetch_files Coordinator[@] "$main_destination" "Coordinator.xctemplate"
+
+echo
+echo -e "\033[32mMoneybox templates fetched and installed in your Xcode\033[0m"
+echo -e "\033[33mRestart Xcode for changes to be applied \033[0m"
